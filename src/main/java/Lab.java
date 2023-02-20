4862a724a403b41a2c29fb5bfdfc2c19d004b15a
Michael Jiles <michaeljilesjr@yahoo.com>
2023-02-20 08:23:02 +0000

    While Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -37,7 +37,11 @@ public class Lab {
         int i = 0;
 
 //        code here
-
+        while (i < end){
+            sum += i;
+            i++;
+            System.out.println(sum);
+        }
         return sum;
     }
 }
